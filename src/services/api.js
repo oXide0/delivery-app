9855eed4c6b997098b138f4d645fefbd1f96fff1
Nazarii Korchevskyi <nazar10fantom@gmail.com>
2023-07-06 22:42:29 +0200

    deleted the database

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
 	reducerPath: 'api',
 	tagTypes: ['User', 'Shop', 'Cart', 'Order'],
-	baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+	baseQuery: fetchBaseQuery({ baseUrl: 'https://delivery-app-server-3lnt.onrender.com' }),
 	endpoints: () => ({}),
 });
